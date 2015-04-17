c974fc9d9d9c4d1d0d6020ff92ef10b540bae454
Sebastian Balay <snbalay@gmail.com>
2015-04-17 13:15:19 -0300

    Project base
    Add gitignore and package.json with basic dependencies and shrinkwrap file
    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/*
+  gulpfile.js
+  ===========
+  Rather than manage one giant configuration file responsible for creating multiple tasks,
+  each task has been broken out into its own file in gulp/tasks.
+  Any files in that directory get automatically required below.
+*/
+
+var requireDir = require('require-dir');
+requireDir('./gulp/tasks', { recurse: true });

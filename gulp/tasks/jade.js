a5a8481cb50482ce1f0bb45b101b12ef52c264b5
Sebastián Balay <snbalay@gmail.com>
2015-06-04 22:35:06 -0300

    Avoid stopping gulp because of jade syntax errors.

--- gulp/tasks/jade.js
+++ gulp/tasks/jade.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    config = require('../config'),
     jade = require('gulp-jade');
 
 gulp.task('jade', function () {
   gulp.src('./src/index.jade')
+    .pipe(plumber({errorHandler: config.errorHandler}))
     .pipe(jade({ pretty : true }))
   .pipe(gulp.dest('./build'));
 
   gulp.src('./src/jade/**/*.jade', { base: 'src' })
+    .pipe(plumber({errorHandler: config.errorHandler}))
     .pipe(jade({ pretty : true }))
   .pipe(gulp.dest('./build/html'));
 });

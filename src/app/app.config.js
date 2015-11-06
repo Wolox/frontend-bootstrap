10868232ae5449eda2f40acc68e8f279ad9bd3e4
Sebastian Balay <snbalay@gmail.com>
2015-11-06 10:53:31 -0300

    add app.config

---
+++ src/app/app.config.js
@@ -0,0 +1,12 @@
+angular.module('app').config([
+  'RestangularProvider', 'configuration', 'localStorageServiceProvider',
+  function (RestangularProvider, configuration, localStorageServiceProvider) {
+
+    // Restangular Setup
+    RestangularProvider.setBaseUrl(configuration.apiUrl);
+    RestangularProvider.setDefaultHeaders({ 'Content-Type': 'application/json' });
+
+    // Local Storage Setup
+    localStorageServiceProvider.setPrefix(window.btoa('app'));
+  }
+]);

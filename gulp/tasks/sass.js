a54619abf959bae4d6d3191d56d44cfea33773ab
SKOLZ <gabriel.zanzotti@wolox.com.ar>
2015-11-13 14:26:49 -0300

    added sass-lint

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     plumber = require('gulp-plumber'),
-    scsslint = require('gulp-scss-lint'),
+    sassLint = require('gulp-sass-lint'),
     sourcemaps = require('gulp-sourcemaps'),
     minifyCss = require('gulp-minify-css'),
     gulpif = require('gulp-if'),
@@ -23,10 +23,11 @@ gulp.task('clean:css', function (cb) {
 gulp.task('sass', ['clean:css'], function () {
   return gulp.src(localConfig.src)
     .pipe(plumber({errorHandler: globalConfig.errorHandler}))
-    .pipe(gulpif(globalConfig.development(), scsslint({
-      'config': 'scss-lint.yml'
+    .pipe(gulpif(globalConfig.development(), sassLint({
+      config: '.sass-lint.yml'
     })))
-    .pipe(gulpif(globalConfig.development(), scsslint.failReporter('E')))
+    .pipe(gulpif(globalConfig.development(), sassLint.format()))
+    .pipe(gulpif(globalConfig.development(), sassLint.failOnError()))
     .pipe(gulpif(globalConfig.development(), sourcemaps.init()))
       .pipe(sass())
       .pipe(concat(localConfig.buildFileName))

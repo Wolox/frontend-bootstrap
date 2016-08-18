143af34f8467e25b0ce14fbc2c2a3ee54a00b992
Sebastian Balay <snbalay@gmail.com>
2016-08-18 10:34:28 -0300

    remove lodash

--- vendorJs.js
+++ vendorJs.js
@@ -2,6 +2,5 @@ module.exports = [
   'angular/angular.js',
   'angular-ui-router/release/angular-ui-router.js',
   'angular-local-storage/dist/angular-local-storage.js',
-  'lodash/lodash.js',
   'angular-translate/angular-translate.js'
 ];

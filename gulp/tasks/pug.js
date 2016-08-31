c8c6892a945ce9ffe94d38c6d223dab8cf63f583
Sebastian Balay <snbalay@gmail.com>
2016-08-31 09:39:17 -0300

    update jade to pug

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,12 +1,12 @@
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
-import jade from 'gulp-jade';
+import pug from 'gulp-pug';
 import preprocess from 'gulp-preprocess';
 import del from 'del';
 import { errorHandler, getSecretKeys } from '../config';
 
 const localConfig = {
-  src: './src/**/*.jade',
+  src: './src/**/*.pug',
   base: 'src',
   dest: './build',
   cleanSrc: './build/**/*.html'
@@ -16,10 +16,10 @@ gulp.task('clean:html', () => {
   return del([localConfig.cleanSrc]);
 });
 
-gulp.task('jade', ['clean:html'], () => {
+gulp.task('pug', ['clean:html'], () => {
   return gulp.src(localConfig.src, { base: localConfig.base })
     .pipe(plumber({ errorHandler }))
-    .pipe(jade({ pretty: true }))
+    .pipe(pug({ pretty: true }))
     .pipe(preprocess({ context: getSecretKeys() }))
   .pipe(gulp.dest(localConfig.dest));
 });

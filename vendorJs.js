a8603f53133096f6176b2721aad9a0693095417c
Sebastian Balay <snbalay@gmail.com>
2016-08-31 10:06:09 -0300

    pug update

--- vendorJs.js
+++ vendorJs.js
@@ -2,5 +2,5 @@ module.exports = [
   'angular/angular.js',
   'angular-ui-router/release/angular-ui-router.js',
   'angular-local-storage/dist/angular-local-storage.js',
-  'angular-translate/angular-translate.js'
+  'angular-translate/dist/angular-translate.js'
 ];

6eb584b9f93765abd63654607bfab557df450da2
FrankIglesias <frank.beatle@gmail.com>
2018-03-10 21:14:52 -0300

    Corrected dependencies

--- vendorJs.js
+++ vendorJs.js
@@ -1,6 +1,6 @@
 module.exports = [
   'angular/angular.js',
-  'angular-ui-router/release/angular-ui-router.js',
+  '@uirouter/angularjs/release/angular-ui-router.js',
   'angular-local-storage/dist/angular-local-storage.js',
   'angular-translate/dist/angular-translate.js'
 ];

9ecb2f3818d4853c61154dc16ed0911aa75a430e
jjuli93 <alvarezjulianignacio@gmail.com>
2018-10-02 17:45:17 -0300

    changes in templates

--- templates/component/{name_pc}.vue
+++ templates/component/{name_pc}.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-      }
+export default {
+  data() {
+    return {
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
